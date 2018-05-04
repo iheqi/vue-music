10d5b4289a496497782bef564b763865344c0a89
rubbish-man <993284758@qq.com>
2018-05-04 15:06:35 +0800

    play btn

--- src/components/music-list/MusicList.vue
+++ src/components/music-list/MusicList.vue
@@ -6,17 +6,28 @@
       </div>
       <h1 class="title" v-html="title"></h1>
     </header>
-    <div class="bg-img" :style="bgStyle" ref="bgImg"></div>
+    <div class="bg-img" :style="bgStyle" ref="bgImg">
+      <div class="play-wrapper" v-show="songs.length">
+        <div class="play">
+          <span class="iconfont play-icon">&#xe63a;</span>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+
+      <div class="filter" ref="filter"></div>
+    </div>
 
     <div class="song-list-wrapper" ref="songWrapper" @pullingDown='scaleImg'>
       <song-list :songs='songs'></song-list>
     </div>
+    <loading v-show="!songs.length"></loading>
   </div>
 </template>
 
 <script>
 import SongList from '../song-list/SongList'
 import Bscroll from 'better-scroll'
+import Loading from '@/components/loading/Loading'
 
 export default {
   name: 'MusicList',
@@ -26,7 +37,8 @@ export default {
     title: String
   },
   components: {
-    SongList
+    SongList,
+    Loading
   },
   data () {
     return {
@@ -50,8 +62,6 @@ export default {
         /* this.timer = setTimeout(() => {
         }, 10) */
       }
-      
-      console.log(scale)
     },
     scaleOneImg () {
       this.$refs.bgImg.style['transform'] = `scale(1)`
@@ -98,9 +108,36 @@ export default {
         background-size: cover
         z-index : 8
         transition : all 0.2s
+        .play-wrapper
+          position: absolute
+          bottom: .4rem
+          width: 100%
+          z-index : 9
+          .play
+            width: 2.7rem
+            height : .64rem
+            margin : 0 auto
+            text-align: center
+            line-height : .64rem
+            border: 1px solid $color-theme
+            color: $color-theme
+            border-radius: 2rem
+            .play-icon
+              font-size: $font-size-large-x
+            .text
+              display: inline-block
+              vertical-align: middle
+              padding-bottom : .2rem              
+        .filter
+          position: absolute
+          top: 0
+          left: 0
+          right: 0
+          bottom: 0
+          background: rgba(7, 17, 27, 0.4)
       .song-list-wrapper
         position: fixed
-        padding-top : 4rem 
+        padding-top : 4.2rem 
         top: .9rem
         bottom: 0
         width: 100%
