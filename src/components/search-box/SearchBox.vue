201e59503b4430adfb1aa0b7449e9703bfea96e3
rubbish-man <993284758@qq.com>
2018-05-13 22:36:39 +0800

    search box

---
+++ src/components/search-box/SearchBox.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="search-box">
+    <i class="iconfont icon-search"></i>
+    <input class="box" :placeholder="placeholder" v-model="query"/>
+    <i class="iconfont icon-dismiss" v-show="query" @click="clear">&#xe608;</i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBox',
+  props: {
+    placeholder: {
+      type: String,
+      default: '搜索歌曲、歌手'
+    }
+  },
+  data() {
+    return {
+      query: '',
+    }
+  },
+  methods: {
+    clear() {
+      this.query = ''
+    }
+  },
+  created () {
+    this.$watch('query', (newVal) => {    // 为何不直接监听
+      this.$emit('query', newVal)
+    })
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/variables"
+    .search-box
+      display : flex
+      background: $color-highlight-background
+      width: 100%
+      height: .8rem
+      padding: 0 .12rem
+      align-items: center
+      box-sizing: border-box
+      border-radius : 0.12rem
+      
+      .icon-search
+        font-size: .42rem
+        color: $color-background
+      .box
+        flex: 1
+        margin: 0 .1rem
+        line-height: .36rem
+        background: $color-highlight-background
+        color: $color-text-l
+        font-size: $font-size-medium
+        &::placeholder    // 
+          color: $color-text-d
+          font-size : .26rem
+      .icon-dismiss
+        font-size: .4rem
+        color: $color-background
+
+</style>
--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>Search</div>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box></search-box>
+    </div>
+  </div>
+  
 </template>
 <script>
+
+import SearchBox from '@/components/search-box/SearchBox'
+
 export default {
-  name: 'Search'
+  name: 'Search',
+  components: {
+    SearchBox
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  @import "~styles/variables"
+    .search-box-wrapper
+      margin : .4rem
 </style>
 
